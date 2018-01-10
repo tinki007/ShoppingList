b2d346f291a0576136a6ae9cc0d89bf07cedd0f5
LeonardTheFirst <kacper160@gmail.com>
2018-01-10 13:56:08 +0100

    saving in a file

---
+++ src/main/java/com/shoppinglist/File.java
@@ -0,0 +1,20 @@
+package com.shoppinglist;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class File {
+    int i = 1;
+
+    public void createFile(ArrayList<String> shoppinglist) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("shoppinglist.txt"));
+        for (String line : shoppinglist) {
+            bufferedWriter.write(i + "  " + line + "\n");
+            i++;
+        }
+        bufferedWriter.close();
+    }
+}
+
